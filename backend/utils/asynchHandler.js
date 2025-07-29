6cd02a410502474ad532f59cf046508a5f2bb17b
Vipransh <vipransh231527@gmail.com>
2025-07-29 12:32:23 +0530

    Initial Commit

--- backend/utils/asynchHandler.js
+++ backend/utils/asynchHandler.js
@@ -1,7 +1,13 @@
+// const asynchHandler = (fn) => (req, res, next) => {
+//     return (req, res, next) => {
+//         Promise.resolve(fn(req, res, next)).catch(next);
+//     }
+// }
+
+// export default asynchHandler;
+
 const asynchHandler = (fn) => (req, res, next) => {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    }
+    Promise.resolve(fn(req, res, next)).catch(next);
 }
 
-export default asynchHandler;
+export default asynchHandler;
